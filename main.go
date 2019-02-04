d58e1a279447e2d346036b6456d013a688e87f0a
Ayub Malik <ayub.malik@gmail.com>
2019-02-04 20:41:17 +0000

    Add log to console for docker

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 )
 
 // Motd represents a Message of the Day
@@ -12,12 +14,14 @@ type Motd struct {
 }
 
 func motdHandler(w http.ResponseWriter, r *http.Request) {
-	motd := Motd{"1.0.0", "hello world!"}
+	motd := Motd{"1.0.1", "hello world!"}
 	w.Header().Set("Content-Type", "application/json")
+	log.Printf("motd: %v", motd)
 	json.NewEncoder(w).Encode(motd)
 }
 
 func main() {
+	log.SetOutput(os.Stdout)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/motd", motdHandler)
 	http.ListenAndServe(":8000", mux)
